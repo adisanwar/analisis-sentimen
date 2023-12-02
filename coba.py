842aa51fb6e43bea33238ce16b5bc79e4db15cf8
synysterRev <69097348+synysterR3v@users.noreply.github.com>
2023-12-02 10:43:32 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -5,8 +5,8 @@ url = "https://twitter-api45.p.rapidapi.com/timeline.php"
 querystring = {"screenname":"polisiidol"}
 
 headers = {
-	"X-RapidAPI-Key": "5d6dc34b2dmsh972584ee74c36dbp116949jsn1a45ba4a8b30",
-	"X-RapidAPI-Host": "twitter-api45.p.rapidapi.com"
+	"X-RapidAPI-Key": "",
+	"X-RapidAPI-Host": ""
 }
 
 response = requests.get(url, headers=headers, params=querystring)
